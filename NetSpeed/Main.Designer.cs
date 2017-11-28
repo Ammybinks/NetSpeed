f31067b549d31580b63ccd62a20acfdc425a7969
Ammybinks <nye@blythe.plus.com>
2017-11-28 14:13:52 +0000

    Initial commit

---
+++ NetSpeed/Main.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace NetSpeed
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TimeButton = new System.Windows.Forms.Button();
+            this.TimeTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.CommitButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CommitList = new System.Windows.Forms.ListView();
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Down = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Up = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Comment = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Time";
+            // 
+            // TimeButton
+            // 
+            this.TimeButton.Location = new System.Drawing.Point(49, 8);
+            this.TimeButton.Name = "TimeButton";
+            this.TimeButton.Size = new System.Drawing.Size(95, 23);
+            this.TimeButton.TabIndex = 2;
+            this.TimeButton.Text = "Get Current Time";
+            this.TimeButton.UseVisualStyleBackColor = true;
+            this.TimeButton.Click += new System.EventHandler(this.TimeButton_Click);
+            // 
+            // TimeTextBox
+            // 
+            this.TimeTextBox.AccessibleName = "TimeTextBox";
+            this.TimeTextBox.Location = new System.Drawing.Point(49, 37);
+            this.TimeTextBox.Name = "TimeTextBox";
+            this.TimeTextBox.Size = new System.Drawing.Size(95, 20);
+            this.TimeTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Down";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Up";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(36, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(36, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Previous Commits";
+            // 
+            // textBox3
+            // 
+            this.textBox3.AllowDrop = true;
+            this.textBox3.Location = new System.Drawing.Point(150, 37);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(285, 81);
+            this.textBox3.TabIndex = 10;
+            // 
+            // CommitButton
+            // 
+            this.CommitButton.Location = new System.Drawing.Point(91, 72);
+            this.CommitButton.Name = "CommitButton";
+            this.CommitButton.Size = new System.Drawing.Size(53, 46);
+            this.CommitButton.TabIndex = 11;
+            this.CommitButton.Text = "Commit";
+            this.CommitButton.UseVisualStyleBackColor = true;
+            this.CommitButton.Click += new System.EventHandler(this.CommitButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(348, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Comment";
+            // 
+            // CommitList
+            // 
+            this.CommitList.CheckBoxes = true;
+            this.CommitList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Time,
+            this.Down,
+            this.Up,
+            this.Comment});
+            this.CommitList.Location = new System.Drawing.Point(12, 143);
+            this.CommitList.Name = "CommitList";
+            this.CommitList.Size = new System.Drawing.Size(426, 111);
+            this.CommitList.TabIndex = 15;
+            this.CommitList.UseCompatibleStateImageBehavior = false;
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            // 
+            // Down
+            // 
+            this.Down.Text = "Down";
+            // 
+            // Up
+            // 
+            this.Up.Text = "Up";
+            // 
+            // Comment
+            // 
+            this.Comment.Text = "Comment";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 266);
+            this.Controls.Add(this.CommitList);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.CommitButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TimeTextBox);
+            this.Controls.Add(this.TimeButton);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Listimagi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button TimeButton;
+        private System.Windows.Forms.TextBox TimeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button CommitButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView CommitList;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.ColumnHeader Down;
+        private System.Windows.Forms.ColumnHeader Up;
+        private System.Windows.Forms.ColumnHeader Comment;
+    }
+}
+
---
+++ NetSpeed/Main.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NetSpeed
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            TimeTextBox.Text = GetCurrentTime();
+
+            CommitList.Columns.Add("Time", 100);
+            CommitList.Columns.Add("Down", 40);
+            CommitList.Columns.Add("Up", 40);
+            CommitList.Columns.Add("Comment", 150);
+        }
+
+        private void TimeButton_Click(object sender, EventArgs e)
+        {
+            TimeTextBox.Text = GetCurrentTime();
+        }
+
+        private void CommitButton_Click(object sender, EventArgs e)
+        {
+            string[] array = new string[4];
+            array[0] = "4PM";
+            array[1] = "0.01";
+            array[2] = "4.63";
+            array[3] = "This is the comment field";
+            CommitList.Items.Add(new ListViewItem(array));
+        }
+
+
+
+        public string GetCurrentTime()
+        {
+            DateTime currentTime;
+            string workingString;
+            string temp;
+
+            currentTime = DateTime.Now;
+
+            temp = currentTime.Hour.ToString();
+
+            if (currentTime.Hour < 10)
+            {
+                temp = "0" + temp;
+            }
+
+            workingString = temp;
+
+            temp = currentTime.Minute.ToString();
+
+            if (currentTime.Minute < 10)
+            {
+                temp = "0" + temp;
+            }
+
+            workingString += ":" + temp;
+
+            temp = currentTime.Day.ToString();
+
+            if (currentTime.Day < 10)
+            {
+                temp = "0" + temp;
+            }
+
+            workingString += " " + temp;
+
+            temp = currentTime.Month.ToString();
+
+            if (currentTime.Month < 10)
+            {
+                temp = "0" + temp;
+            }
+
+            workingString += "/" + temp;
+
+            workingString += "/" + currentTime.Year.ToString().Substring(2);
+
+            return workingString;
+        }
+    }
+}
